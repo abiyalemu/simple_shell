0f6882416790dcd357ac011a1fe959ea44e20840
Tihitna-22 <tihitna222sisay@gmail.com>
2021-11-03 05:09:29 -0700

    add shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef "SHELL_H"
+#define "SHELL_H"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main();
+
+
+
+#endif
